c2c5dad67fd9e0e1bd1ce312fc5fb2fc9fab28f6
Dhimas <dimasalvi77@gmail.com>
2020-05-15 20:44:51 +0700

    third commit

---
+++ Polymorphisminterface/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PolymorphismInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama\t: Dhimas Alvi Hidayah ");
+            Console.WriteLine("NIM\t: 19.11.2805");
+            Console.WriteLine("Kelas\t: 19 IF 04");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pilih Printer: \n1. Epson\n2. Canon\n3. LaserJet");
+
+            Console.Write("\nNomor Printer [1 ... 3] : ");
+            int pilihPrinter = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            IPrinterWindows printer = new PrinterWindows();
+
+            if (pilihPrinter > 0 && pilihPrinter < 4)
+            {
+                if (pilihPrinter == 1)
+                {
+                    printer = new Epson();
+                }
+                else if (pilihPrinter == 2)
+                {
+                    printer = new Canon();
+                }
+                else
+                {
+                    printer = new LaserJet();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Pilihan tidak valid \n");
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
